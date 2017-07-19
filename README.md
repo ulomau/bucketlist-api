36a60d9f26ceba71528fa5ff6abf0db46ecd63b7
John O. Paul <oyomthojohnpaul@gmail.com>
2017-07-19 21:56:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bucketlist API (Challenge 3)
+
+Keep track of your goals or dreams. Don't forget to change the world.
---
+++ run.py
@@ -0,0 +1,5 @@
+from bucketlist.models import User
+from bucketlist.app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
