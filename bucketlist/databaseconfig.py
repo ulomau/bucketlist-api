1818cf61943ad9de3f44831022db18b1ee056733
John O. Paul <oyomthojohnpaul@gmail.com>
2017-07-31 16:27:33 +0300

    Move database configuration to environment path
    The database configuration(connection string) is retrieved from the
    environment. If none is available, a preset is used.

--- bucketlist/databaseconfig.py
+++ bucketlist/databaseconfig.py
@@ -1,3 +1,7 @@
 """This module contains database configurations for the app"""
+import os
 
-DATABASE_URI = 'postgresql://postgres:su@postgressdb@localhost/bucketlist'
+DATABASE_URI = os.environ.get('DATABASE_URI')
+
+if not DATABASE_URI:
+    DATABASE_URI = 'postgresql://postgres:su@postgressdb@localhost/bucketlist'
