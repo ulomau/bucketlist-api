42ffee3ee3fbeb6a2e34aaec291eed904d526505
John O. Paul <oyomthojohnpaul@gmail.com>
2017-08-02 08:39:50 +0300

    Change database configuration name
    From DATABASE_URI to DATABASE_URL

--- bucketlist/databaseconfig.py
+++ bucketlist/databaseconfig.py
@@ -1,7 +1,4 @@
 """This module contains database configurations for the app"""
 import os
 
-DATABASE_URI = os.environ.get('DATABASE_URI')
-
-if not DATABASE_URI:
-    DATABASE_URI = 'postgresql://postgres:su@postgressdb@localhost/bucketlist'
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:su@postgressdb@localhost/bucketlist')
